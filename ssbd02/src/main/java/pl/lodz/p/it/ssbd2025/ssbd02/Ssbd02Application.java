bf681b02036bbfa94e60fe5df0aefb2c285dd8f9
Wojciech Florczak <florczak.wojciechv@gmail.com>
2025-03-18 16:35:13 +0100

    utworzenie pierwszej wersji projektu

---
+++ ssbd02/src/main/java/pl/lodz/p/it/ssbd2025/ssbd02/Ssbd02Application.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.ssbd2025.ssbd02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssbd02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssbd02Application.class, args);
+	}
+
+}
