62350c41df883c7a2b199742d4fdeabcf6bd0436
Bartosz Podemski <247766@edu.p.lodz.pl>
2025-05-08 23:18:09 +0000

    Resolve "MOK.15 poprawki"

--- src/main/java/pl/lodz/p/it/ssbd2025/ssbd02/dto/AccountWithTokenDTO.java
+++ src/main/java/pl/lodz/p/it/ssbd2025/ssbd02/dto/AccountWithTokenDTO.java
@@ -1,7 +1,10 @@
 package pl.lodz.p.it.ssbd2025.ssbd02.dto;
 
+import java.util.Collection;
+
 public record AccountWithTokenDTO(
         AccountReadDTO account,
-        String lockToken
+        String lockToken,
+        Collection<AccountRolesProjection> roles
 ) {
 }
