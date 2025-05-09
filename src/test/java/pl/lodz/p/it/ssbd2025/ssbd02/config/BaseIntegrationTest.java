5fabf3f267c8038b1266fe4025783dc6f23f04cc
Bartosz Podemski <247766@edu.p.lodz.pl>
2025-05-09 18:06:38 +0000

    test containers for tests

---
+++ src/test/java/pl/lodz/p/it/ssbd2025/ssbd02/config/BaseIntegrationTest.java
@@ -0,0 +1,33 @@
+package pl.lodz.p.it.ssbd2025.ssbd02.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class BaseIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.4")
+            .withDatabaseName("ssbd")
+            .withUsername("ssbd02admin")
+            .withPassword("magn0lia");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("app.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.admin.username", postgres::getUsername);
+        registry.add("spring.datasource.admin.password", postgres::getPassword);
+
+        registry.add("spring.datasource.mok.username", () -> "ssbd02mok");
+        registry.add("spring.datasource.mok.password", () -> "P@ssw0rd");
+        registry.add("spring.datasource.mod.username", () -> "ssbd02mod");
+        registry.add("spring.datasource.mod.password", () -> "P@ssw0rd");
+    }
+}
---
+++ src/test/java/pl/lodz/p/it/ssbd2025/ssbd02/integration/testowyTest.java
@@ -0,0 +1,64 @@
+package pl.lodz.p.it.ssbd2025.ssbd02.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import pl.lodz.p.it.ssbd2025.ssbd02.config.BaseIntegrationTest;
+import pl.lodz.p.it.ssbd2025.ssbd02.dto.AccountDTO;
+import pl.lodz.p.it.ssbd2025.ssbd02.dto.ClientDTO;
+import pl.lodz.p.it.ssbd2025.ssbd02.dto.UserRoleDTO;
+import pl.lodz.p.it.ssbd2025.ssbd02.enums.Language;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+public class testowyTest extends BaseIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void test() throws Exception {
+        UserRoleDTO.ClientDTO clientDTO = new UserRoleDTO.ClientDTO();
+
+        AccountDTO accountDTO = new AccountDTO(
+                null,
+                null,
+                "testuser",
+                "P@ssw0rd!",
+                null,
+                null,
+                "Joe",
+                "Doe",
+                "joe.doe@gmail.com",
+                null,
+                null,
+                Language.pl_PL,
+                null,
+                null
+        );
+
+        ClientDTO clientDTO2 = new ClientDTO(clientDTO, accountDTO);
+
+        String requestJson = objectMapper.writeValueAsString(clientDTO2);
+
+        mockMvc.perform(post("/api/client/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.account.login").value("testuser"))
+                .andExpect(jsonPath("$.account.firstName").value("Joe"));
+    }
+}
