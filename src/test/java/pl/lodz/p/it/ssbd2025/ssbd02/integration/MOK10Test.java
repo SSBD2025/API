b0e6116d5f60097399ff1274d6ac12bc2d612b6e
Wojciech Florczak <237370@edu.p.lodz.pl>
2025-05-15 21:44:49 +0000

    Resolve "more tests mok10, mok11"

--- src/test/java/pl/lodz/p/it/ssbd2025/ssbd02/integration/MOK10Test.java
+++ src/test/java/pl/lodz/p/it/ssbd2025/ssbd02/integration/MOK10Test.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -17,7 +16,6 @@ import pl.lodz.p.it.ssbd2025.ssbd02.dto.*;
 import pl.lodz.p.it.ssbd2025.ssbd02.entities.TokenEntity;
 import pl.lodz.p.it.ssbd2025.ssbd02.enums.TokenType;
 import pl.lodz.p.it.ssbd2025.ssbd02.mok.repository.TokenRepository;
-import pl.lodz.p.it.ssbd2025.ssbd02.mok.service.implementations.AccountService;
 import pl.lodz.p.it.ssbd2025.ssbd02.mok.service.interfaces.IAccountService;
 import pl.lodz.p.it.ssbd2025.ssbd02.utils.JwtTokenProvider;
 
@@ -45,76 +43,119 @@ public class MOK10Test extends BaseIntegrationTest {
     @Autowired
     private IAccountService accountService;
 
+    private String accessToken;
+
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        mockMvc.perform(post("/api/account/logout")
+                .header("Authorization", "Bearer " + accessToken)).andReturn();
+
         tokenRepository.deleteAll();
     }
 
-//    @Test
-//    public void changeOwnEmailPositiveTest() throws Exception {
-//        LoginDTO loginDTO = new LoginDTO(
-//                "drice",
-//                "P@ssw0rd!"
-//        );
-//
-//        String json = objectMapper.writeValueAsString(loginDTO);
-//
-//        MvcResult result = mockMvc.perform(post("/api/account/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(json))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        String responseBody = result.getResponse().getContentAsString();
-//
-//        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
-//
-//        String accessToken = tokenPair.accessToken();
-//
-//        ChangeEmailDTO changeEmailDTO = new ChangeEmailDTO("newemail@example.com");
-//
-//        String changeEmailJson = objectMapper.writeValueAsString(changeEmailDTO);
-//
-//        mockMvc.perform(post("/api/account/change-email")
-//                        .header("Authorization", "Bearer " + accessToken)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(changeEmailJson))
-//                .andExpect(status().isOk());
-//
-//        TokenEntity token = tokenRepository.findAll().stream()
-//                .filter(t -> t.getType() == TokenType.EMAIL_CHANGE)
-//                .findFirst()
-//                .orElseThrow();
-//
-//        Assertions.assertNotNull(token);
-//        Assertions.assertEquals(TokenType.EMAIL_CHANGE, token.getType());
-//        Assertions.assertEquals("newemail@example.com", jwtTokenProvider.getNewEmailFromToken(token.getToken()));
-//
-//        mockMvc.perform(get("/api/account/confirm-email")
-//                        .param("token", token.getToken()))
-//                .andExpect(status().isOk());
-//
-//        Assertions.assertEquals("newemail@example.com", accountService.getAccountByLogin("drice").account().email());
-//    }
-
     @Test
-    @WithMockUser
-    public void changeOwnEmailNewEmailTooLongTest() throws Exception {
-        ChangeEmailDTO dto = new ChangeEmailDTO("verylongemailaddress1234567890verylongemailaddress1234567890verylongemailaddress1234567890@example.com");
-        String json = objectMapper.writeValueAsString(dto);
+    public void changeOwnEmailPositiveTest() throws Exception {
+        LoginDTO loginDTO = new LoginDTO(
+                "drice",
+                "P@ssw0rd!"
+        );
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/account/change-email")
+        String json = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mockMvc.perform(post("/api/account/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
+
+        accessToken = tokenPair.accessToken();
+
+        ChangeEmailDTO changeEmailDTO = new ChangeEmailDTO("newemail@example.com");
+
+        String changeEmailJson = objectMapper.writeValueAsString(changeEmailDTO);
+
+        mockMvc.perform(post("/api/account/change-email")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(changeEmailJson))
+                .andExpect(status().isOk());
+
+        TokenEntity token = tokenRepository.findAll().stream()
+                .filter(t -> t.getType() == TokenType.EMAIL_CHANGE)
+                .findFirst()
+                .orElseThrow();
+
+        Assertions.assertNotNull(token);
+        Assertions.assertEquals(TokenType.EMAIL_CHANGE, token.getType());
+        Assertions.assertEquals("newemail@example.com", jwtTokenProvider.getNewEmailFromToken(token.getToken()));
+
+        mockMvc.perform(get("/api/account/confirm-email")
+                        .param("token", token.getToken()))
+                .andExpect(status().isOk());
+
+        Assertions.assertEquals("newemail@example.com", accountService.getAccountByLogin("drice").account().email());
+    }
+
+    @Test
+    public void changeOwnEmailNewEmailTooLongTest() throws Exception {
+        LoginDTO loginDTO = new LoginDTO(
+                "drice",
+                "P@ssw0rd!"
+        );
+
+        String json = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mockMvc.perform(post("/api/account/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
+
+        accessToken = tokenPair.accessToken();
+
+        ChangeEmailDTO dto = new ChangeEmailDTO("verylongemailaddress1234567890verylongemailaddress1234567890verylongemailaddress1234567890@example.com");
+        String changeEmail = objectMapper.writeValueAsString(dto);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/account/change-email")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(changeEmail))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    @WithMockUser
     public void changeOwnEmailWhenEmailInvalidFormatTest() throws Exception {
+        LoginDTO loginDTO = new LoginDTO(
+                "drice",
+                "P@ssw0rd!"
+        );
+
+        String json = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mockMvc.perform(post("/api/account/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
+
+        accessToken = tokenPair.accessToken();
+
         ChangeEmailDTO dto = new ChangeEmailDTO("not_an_email");
 
         mockMvc.perform(post("/api/account/change-email")
+                        .header("Authorization", "Bearer " + accessToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isBadRequest());
@@ -133,11 +174,30 @@ public class MOK10Test extends BaseIntegrationTest {
     }
 
     @Test
-    @WithMockUser
     public void changeOwnEmailEmailIsBlankTest() throws Exception {
+        LoginDTO loginDTO = new LoginDTO(
+                "drice",
+                "P@ssw0rd!"
+        );
+
+        String json = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mockMvc.perform(post("/api/account/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
+
+        accessToken = tokenPair.accessToken();
+
         ChangeEmailDTO dto = new ChangeEmailDTO("");
 
         mockMvc.perform(post("/api/account/change-email")
+                        .header("Authorization", "Bearer " + accessToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(dto)))
                 .andExpect(status().isBadRequest());
@@ -155,7 +215,7 @@ public class MOK10Test extends BaseIntegrationTest {
                 .andReturn();
 
         TokenPairDTO tokenPair = objectMapper.readValue(loginResult.getResponse().getContentAsString(), TokenPairDTO.class);
-        String accessToken = tokenPair.accessToken();
+        accessToken = tokenPair.accessToken();
 
         ChangeEmailDTO sameEmail = new ChangeEmailDTO("tcheese@example.com");
         String json = objectMapper.writeValueAsString(sameEmail);
@@ -182,7 +242,7 @@ public class MOK10Test extends BaseIntegrationTest {
                 .andReturn();
 
         TokenPairDTO tokenPair = objectMapper.readValue(loginResult.getResponse().getContentAsString(), TokenPairDTO.class);
-        String accessToken = tokenPair.accessToken();
+        accessToken = tokenPair.accessToken();
 
         ChangeEmailDTO takenEmail = new ChangeEmailDTO("jcheddar@example.com");
         String takenEmailJson = objectMapper.writeValueAsString(takenEmail);
@@ -194,64 +254,64 @@ public class MOK10Test extends BaseIntegrationTest {
                 .andExpect(status().isConflict());
     }
 
-//    @Test
-//    public void revertEmailChangePositiveTest() throws Exception {
-//        LoginDTO loginDTO = new LoginDTO(
-//                "tcheese",
-//                "P@ssw0rd!"
-//        );
-//
-//        String json = objectMapper.writeValueAsString(loginDTO);
-//
-//        MvcResult result = mockMvc.perform(post("/api/account/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(json))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        String responseBody = result.getResponse().getContentAsString();
-//
-//        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
-//
-//        String accessToken = tokenPair.accessToken();
-//
-//        ChangeEmailDTO changeEmailDTO = new ChangeEmailDTO("changeEmail@example.com");
-//
-//        String changeEmailJson = objectMapper.writeValueAsString(changeEmailDTO);
-//
-//        mockMvc.perform(post("/api/account/change-email")
-//                        .header("Authorization", "Bearer " + accessToken)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(changeEmailJson))
-//                .andExpect(status().isOk());
-//
-//        TokenEntity token = tokenRepository.findAll().stream()
-//                .filter(t -> t.getType() == TokenType.EMAIL_CHANGE)
-//                .findFirst()
-//                .orElseThrow();
-//
-//        Assertions.assertNotNull(token);
-//        Assertions.assertEquals(TokenType.EMAIL_CHANGE, token.getType());
-//        Assertions.assertEquals("changeEmail@example.com", jwtTokenProvider.getNewEmailFromToken(token.getToken()));
-//
-//        mockMvc.perform(get("/api/account/confirm-email")
-//                        .param("token", token.getToken()))
-//                .andExpect(status().isOk());
-//
-//        TokenEntity revertToken = tokenRepository.findAll().stream()
-//                .filter(t -> t.getType() == TokenType.EMAIL_REVERT)
-//                .findFirst()
-//                .orElseThrow();
-//
-//        Assertions.assertNotNull(revertToken);
-//        Assertions.assertEquals(TokenType.EMAIL_REVERT, revertToken.getType());
-//
-//        mockMvc.perform(get("/api/account/revert-email-change")
-//                        .param("token", revertToken.getToken()))
-//                .andExpect(status().isOk());
-//
-//        Assertions.assertEquals("tcheese@example.com", accountService.getAccountByLogin(loginDTO.getLogin()).account().email());
-//    }
+    @Test
+    public void revertEmailChangePositiveTest() throws Exception {
+        LoginDTO loginDTO = new LoginDTO(
+                "tcheese",
+                "P@ssw0rd!"
+        );
+
+        String json = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mockMvc.perform(post("/api/account/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
+
+        accessToken = tokenPair.accessToken();
+
+        ChangeEmailDTO changeEmailDTO = new ChangeEmailDTO("changeEmail@example.com");
+
+        String changeEmailJson = objectMapper.writeValueAsString(changeEmailDTO);
+
+        mockMvc.perform(post("/api/account/change-email")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(changeEmailJson))
+                .andExpect(status().isOk());
+
+        TokenEntity token = tokenRepository.findAll().stream()
+                .filter(t -> t.getType() == TokenType.EMAIL_CHANGE)
+                .findFirst()
+                .orElseThrow();
+
+        Assertions.assertNotNull(token);
+        Assertions.assertEquals(TokenType.EMAIL_CHANGE, token.getType());
+        Assertions.assertEquals("changeEmail@example.com", jwtTokenProvider.getNewEmailFromToken(token.getToken()));
+
+        mockMvc.perform(get("/api/account/confirm-email")
+                        .param("token", token.getToken()))
+                .andExpect(status().isOk());
+
+        TokenEntity revertToken = tokenRepository.findAll().stream()
+                .filter(t -> t.getType() == TokenType.EMAIL_REVERT)
+                .findFirst()
+                .orElseThrow();
+
+        Assertions.assertNotNull(revertToken);
+        Assertions.assertEquals(TokenType.EMAIL_REVERT, revertToken.getType());
+
+        mockMvc.perform(get("/api/account/revert-email-change")
+                        .param("token", revertToken.getToken()))
+                .andExpect(status().isOk());
+
+        Assertions.assertEquals("tcheese@example.com", accountService.getAccountByLogin(loginDTO.getLogin()).account().email());
+    }
 
     @Test
     public void revertEmailChangeTokenNotFoundTest() throws Exception {
@@ -262,42 +322,42 @@ public class MOK10Test extends BaseIntegrationTest {
                 .andExpect(status().isNotFound());
     }
 
-//    @Test
-//    public void resendEmailChangeLinkPositiveTest() throws Exception {
-//        LoginDTO loginDTO = new LoginDTO(
-//                "drice",
-//                "P@ssw0rd!"
-//        );
-//
-//        String json = objectMapper.writeValueAsString(loginDTO);
-//
-//        MvcResult result = mockMvc.perform(post("/api/account/login")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(json))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        String responseBody = result.getResponse().getContentAsString();
-//
-//        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
-//
-//        String accessToken = tokenPair.accessToken();
-//
-//        ChangeEmailDTO changeEmailDTO = new ChangeEmailDTO("adminnewemail@example.com");
-//
-//        String changeEmailJson = objectMapper.writeValueAsString(changeEmailDTO);
-//
-//        mockMvc.perform(post("/api/account/change-email")
-//                        .header("Authorization", "Bearer " + accessToken)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(changeEmailJson))
-//                .andExpect(status().isOk());
-//
-//        mockMvc.perform(post("/api/account/resend-change-email")
-//                        .header("Authorization", "Bearer " + accessToken)
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//    }
+    @Test
+    public void resendEmailChangeLinkPositiveTest() throws Exception {
+        LoginDTO loginDTO = new LoginDTO(
+                "drice",
+                "P@ssw0rd!"
+        );
+
+        String json = objectMapper.writeValueAsString(loginDTO);
+
+        MvcResult result = mockMvc.perform(post("/api/account/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBody = result.getResponse().getContentAsString();
+
+        TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
+
+        accessToken = tokenPair.accessToken();
+
+        ChangeEmailDTO changeEmailDTO = new ChangeEmailDTO("adminnewemail@example.com");
+
+        String changeEmailJson = objectMapper.writeValueAsString(changeEmailDTO);
+
+        mockMvc.perform(post("/api/account/change-email")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(changeEmailJson))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/api/account/resend-change-email")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
     @Test
     public void resendEmailChangeLinkUnauthorizedTest() throws Exception {
@@ -325,7 +385,7 @@ public class MOK10Test extends BaseIntegrationTest {
 
         TokenPairDTO tokenPair = objectMapper.readValue(responseBody, TokenPairDTO.class);
 
-        String accessToken = tokenPair.accessToken();
+        accessToken = tokenPair.accessToken();
 
         mockMvc.perform(post("/api/account/resend-change-email")
                         .header("Authorization", "Bearer " + accessToken)
