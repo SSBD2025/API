bb87cbd50b826b15958027d594427a9a528cbecc
Piotr Lewkowicz <247720@edu.p.lodz.pl>
2025-06-21 10:23:33 +0000

    Resolve "DTO validation"

--- src/main/java/pl/lodz/p/it/ssbd2025/ssbd02/dto/AccountWithTokenDTO.java
+++ src/main/java/pl/lodz/p/it/ssbd2025/ssbd02/dto/AccountWithTokenDTO.java
@@ -1,30 +1,36 @@
 package pl.lodz.p.it.ssbd2025.ssbd02.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
-import org.springframework.validation.annotation.Validated;
-import pl.lodz.p.it.ssbd2025.ssbd02.dto.vgroups.OnRequest;
-import pl.lodz.p.it.ssbd2025.ssbd02.utils.consts.DTOConsts;
-
-import java.util.Collection;
+import pl.lodz.p.it.ssbd2025.ssbd02.utils.consts.AccountConsts;
 
 @Data
 @Setter(AccessLevel.NONE)
 @Getter
 @AllArgsConstructor
-public class AccountWithTokenDTO{
-        //@Validated(OnRequest.class) ?? todo co z tym
-        AccountDTO account;
-        String lockToken;
-        Iterable<AccountRoleDTO> roles;
+public class AccountWithTokenDTO {
+
+        @NotNull(message = AccountConsts.ACCOUNT_NOT_NULL_MESSAGE)
+        @Valid
+        private AccountDTO account;
+
+        private String lockToken;
+
+        @NotNull(message = AccountConsts.ROLES_NOT_NULL_MESSAGE)
+        @NotEmpty(message = AccountConsts.ROLES_NOT_EMPTY_MESSAGE)
+        @Valid
+        private Iterable<@Valid AccountRoleDTO> roles;
 
         private AccountWithTokenDTO() {}
 
         @Override
         public String toString() {
-            return "AccountWithTokenDTO{" +
-                    "account=" + account +
-                    ", lockTokenl='" + DTOConsts.PROTECTED + '\'' +
-                    ", roles=" + roles +
-                    '}';
+                return "AccountWithTokenDTO{" +
+                        "account=" + account +
+                        ", lockTokenl='" + pl.lodz.p.it.ssbd2025.ssbd02.utils.consts.DTOConsts.PROTECTED + '\'' +
+                        ", roles=" + roles +
+                        '}';
         }
 }
